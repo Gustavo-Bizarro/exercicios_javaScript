b12eeb03bc0bfc5adcf43ebf8abe287620a170ca
Gustavo Bizarro Lombardo <90002024+Dev1Bizarro@users.noreply.github.com>
2023-09-11 15:54:09 -0300

    ultimo commit do exercício de Dom

--- 4_DOM/js.js
+++ 4_DOM/js.js
@@ -72,3 +72,81 @@ function filtrarLista(){
 
 const inputFiltro = document.querySelector("#filtro");
 inputFiltro.addEventListener("keyup", filtrarLista)
+
+// - 36 mover itens
+
+function moverItem(direcao){
+    const lista = document.querySelector("#listaMover");
+    const itens = Array.from(lista.querySelectorAll("li"));
+    const itemSelecionado = lista.querySelector(".selecionado");
+
+    if(!itemSelecionado){
+        alert("selecionar um item")
+        return;
+    }
+
+    const index = itens.indexOf(itemSelecionado)
+
+    if(direcao === "cima" && index > 0){
+        lista.insertBefore(itemSelecionado, itens[index -1])
+    }else if(direcao === "baixo" && index < itens.length -1){
+        lista.insertBefore(itemSelecionado.nextElementSibling, itens[index])
+    }
+}
+
+function selecionarItem(event){
+    const itens = document.querySelectorAll("#listaMover li")
+    
+    itens.forEach((item) => item.classList.remove("selecionado"));
+    event.target.classList.add("selecionado")
+}
+
+const itensDaLista = document.querySelectorAll("#listaMover li")
+
+
+itensDaLista.forEach((item) => item.addEventListener("click", selecionarItem))
+
+const botaoCima = document.querySelector("#moverCima")
+const botaoBaixo = document.querySelector("#moverBaixo")
+
+botaoCima.addEventListener("click", () => moverItem("cima"))
+botaoBaixo.addEventListener("click", () => moverItem("baixo"))
+
+// 37 - Modal
+
+function abrirModal(){
+document.querySelector("#myModal").style.display = "block"
+}
+function fecharModal(){
+    document.querySelector("#myModal").style.display = "none"
+}
+
+const abrirBtn = document.querySelector("#abrir")
+const fecharBtn = document.querySelector(".close")
+
+abrirBtn.addEventListener("click" , abrirModal)
+fecharBtn.addEventListener("click" , fecharModal)
+
+
+// 38 - Accordion
+
+function toggleAcordion(event){
+    const header = event.target
+    const content = header.nextElementSibling
+
+if(content.style.display === "none" || content.style.display === ""){
+    closeAllAccordionContent()
+    content.style.display = "block"
+}else{
+    content.style.display = "none"
+ }
+}
+
+function closeAllAccordionContent(){
+    const contents = document.querySelectorAll(".accordion-content") 
+    contents.forEach((content) => (content.style.display = "none"))
+}
+
+const header = document.querySelectorAll(".accordion-header")
+
+header.forEach((header) => header.addEventListener("click", toggleAcordion));
